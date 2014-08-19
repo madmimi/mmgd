810e22343fb4c312d1e1c488cd9cf2dc44506a72
Sascha Michael Trinkaus <sascha@trinkaus.cc>
2014-08-19 15:11:45 +0200

    removes bgcliptext JS and adds sectionHeight logic

--- src/mmgd.js
+++ src/mmgd.js
@@ -2,25 +2,29 @@ document.documentElement.setAttribute('data-ua', navigator.userAgent);
 
 $(document).ready(function() {
 
-  Modernizr.addTest('backgroundcliptext', function() {
-    var div = document.createElement('div');
-    div.style.webkitBackgroundClip = "text";
-    var text = div.style.cssText.indexOf('text');
-    if (text > 0) {
-      return true;
-    }
-    'Webkit Moz O ms Khtml'.replace(/([A-Za-z]*)/g, function(val) {
-      if (val + 'BackgroundClip' in div.style) {
-        return true;
-      }
-    });
+  var firstSection = $('body section:first-of-type');
+
+  function setHeight() {
+    sectionHeight = firstSection.height();
+    console.log(sectionHeight);
+  }
+
+  setHeight();
+
+  var resizeTimer;
+  $(window).resize(function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(setHeight, 500);
   });
-});
+
+}); // end document.ready
+
+
 
 // Init Skrollr
-  var s = skrollr.init({
-      render: function(data) {
-          //Debugging - Log the current scroll position.
-          console.log(data.curTop);
-      }
-  });
+var s = skrollr.init({
+  render: function(data) {
+    //Debugging - Log the current scroll position.
+    // console.log(data.curTop);
+  }
+});
